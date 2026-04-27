338f863d12c5615e7ff0065500a4f127e80677b4
han lee <hanhylee@student.ubc.ca>
2026-04-27 08:17:58 -0700

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        string substring = "";
+        unordered_set<char> substring_chars;
+        int max_len = 0;
+
+        for (const char& c: s) {
+            if (substring_chars.find(c) == substring_chars.end()) {
+                substring_chars.insert(c);
+                substring.push_back(c);
+                max_len = max(max_len, (int) substring.size());
+            } else {
+                while (substring.size() > 0 && substring_chars.find(c) != substring_chars.end()) {
+                    // cout << substring.find(c) << endl;
+                    substring_chars.erase(substring[0]);
+                    substring.erase(0, 1);
+                }
+                substring_chars.insert(c);
+                substring.push_back(c);
+            }
+            cout << substring << endl;
+        }
+
+        return max_len;
+    }
+};
