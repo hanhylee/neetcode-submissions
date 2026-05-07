6b22d59d2ed892b2e1cad3682cfd8b93952c6c1a
han lee <hanhylee@student.ubc.ca>
2026-05-07 15:16:33 -0700

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        templist = []*len(nums)*2
+        for i, item in enumerate(nums): 
+            templist.insert(i, item)
+            templist.insert(i + len(nums), item)
+        
+        return templist        
