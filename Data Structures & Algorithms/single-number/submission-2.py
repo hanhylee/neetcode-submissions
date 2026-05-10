1a3f7af9bf817bd0899fab321197832b43966735
han lee <hanhylee@student.ubc.ca>
2026-05-09 17:39:50 -0700

    Remove: single-number - submission-2

--- Data Structures & Algorithms/single-number/submission-2.py
+++
@@ -1,6 +0,0 @@
-from functools import reduce
-import operator
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        # Runs the XOR operation entirely in C
-        return reduce(operator.xor, nums)
