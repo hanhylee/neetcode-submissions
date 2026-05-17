bf986c53c629c2a422ccda26d72750d6450e9e43
han lee <hanhylee@student.ubc.ca>
2026-05-17 07:54:32 -0700

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.py
@@ -0,0 +1,50 @@
+from collections import defaultdict
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        # given list of UNDIRECTED edges
+        # CONSTRAINT: there is at least 1 edge that causes graph to cycle
+
+        """
+        APPROACH
+        - DFS for cycle detection
+        - build graph once and cycle back in a single dfs
+        """
+        n = len(edges)
+        adj = [[] for _ in range(n+1)]
+        for a, b in edges:
+            adj[a].append(b)
+            adj[b].append(a)
+
+        visited = [0 for i in range(n+1)]
+        cycle   = [0 for i in range(n+1)]
+        cycleStart = -1
+
+        def dfs(curr: int, parent: int) -> bool:
+            nonlocal visited, adj, cycleStart
+            if visited[curr] == 1:
+                cycleStart = curr
+                return True
+
+            visited[curr] = 1
+            for neighbor in adj[curr]:
+                if neighbor == parent:
+                    continue
+                if dfs(neighbor, curr):
+                    if cycleStart != -1:
+                        cycle[curr] = 1
+                    if curr == cycleStart:
+                        cycleStart = -1
+                    return True
+
+            return False
+        
+        dfs(1, -1)
+
+        for a, b in reversed(edges):
+            if cycle[a] and cycle[b]:
+                return [a, b]
+
+        # return the edge that can be removed so the graph is still connected
+        # return the last edge in edges if there are multiple answers
+        return []
